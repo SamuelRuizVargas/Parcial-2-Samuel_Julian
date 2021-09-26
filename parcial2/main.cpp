5315d2590dce85e97d2ae07b0275bbb214166575
Samuel Ruiz Vargas <samuel.ruizv@udea.edu.co>
2021-09-26 16:40:25 -0500

    Re formulacion del main

--- parcial2/main.cpp
+++ parcial2/main.cpp
@@ -6,6 +6,28 @@ using namespace std;
 
 int main()
 {
+    string nom_img;
+    cout<<"NOTA: el programa no admite imagenes demasiado grandes y solo acepta imagenes .jpg"<<endl<<endl;
+    cout<<"Ingrese el nombre de su archivo(con extension): ";cin>>nom_img;
+    nom_img=PATH_IM+nom_img;
     imagen im=imagen();
+    int datos_r[16][16];//valores de R por pixel
+    int datos_g[16][16];//valores de G por pixel
+    int datos_b[16][16];//valores de B por pixel
+    int comparador=16*16;
+    int ancho=im.getwidth(nom_img),alto=im.getheight(nom_img);
+    if(ancho*alto>comparador)//Algoritmo de submuestreo
+    {
+        im.submuestreo(datos_r,datos_g,datos_b,nom_img);
+    }
+    else if(ancho*alto<comparador)//Algoritmo de sobremuestreo
+    {
+        im.sobremuestreo(datos_r,datos_g,datos_b,nom_img);
+    }
+    else //simplemente enviar informacion cruda
+    {
+        im.no_muestreo(datos_r,datos_g,datos_b,nom_img);
+    }
+    im.guardar(datos_r,datos_g,datos_b);
     return 0;
 }
